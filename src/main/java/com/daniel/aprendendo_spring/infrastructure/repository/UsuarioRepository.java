79a43dc360f618614343fe66b83f4513346748f8
Daniel <danielchini03@gmail.com>
2026-04-17 10:26:59 -0300

    Adicionando o projeto completo

---
+++ src/main/java/com/daniel/aprendendo_spring/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,21 @@
+package com.daniel.aprendendo_spring.infrastructure.repository;
+
+import com.daniel.aprendendo_spring.infrastructure.entity.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+
+}
